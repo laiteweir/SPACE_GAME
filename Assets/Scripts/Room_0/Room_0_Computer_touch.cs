192aedde7b5bf50315e1db8d1685190adfc5b6da
laiteweir <85702611+laiteweir@users.noreply.github.com>
2022-11-03 05:32:14 +0800

    11/3 Finish talk coroutine

--- Assets/Scripts/Room_0/Room_0_Computer_touch.cs
+++ Assets/Scripts/Room_0/Room_0_Computer_touch.cs
@@ -5,15 +5,16 @@ using UnityEngine.Rendering.Universal;
 
 public class Room_0_Computer_touch : Keyitem
 {
-    new Collider2D collider;
-    GameObject computer_light;
+    //new Collider2D collider;
+    [SerializeField] GameObject computer_light;
+    private TextAsset dialog01;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<Collider2D>();
-        computer_light = GameObject.Find("Robot_01_Light_1");
+        //collider = GetComponent<Collider2D>();
+        dialog01 = Resources.Load<TextAsset>("dialog");
 
     }
 
@@ -31,10 +32,7 @@ public class Room_0_Computer_touch : Keyitem
         // Debug.Log("touch robot 01 in room 0");
         Manager.Instance.ui.SetActive(true);
         Manager.Instance.dialogBox.TextIsOn = true;
-        while(Manager.Instance.dialogBox.TextIsOn){
-            new WaitForSeconds(1);
-        }
-        Debug.Log("exit");
+        Manager.Instance.ui.GetComponent<ReadText>().StartTalk(dialog01);
     }
     public override void EndKeyitemEvent()
     {
