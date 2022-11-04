df792cff8de2d6175b4f6c7b2eac757044f14ad4
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-04 22:50:35 +0800

    create game object and check item

---
+++ Assets/Scripts/CheckItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string keyword;
+    public bool exist = false;
+    [SerializeField] Inventory mybag;
+    void Update(){
+        Check();
+    }
+    void Check(){
+        for(int i=0; i<mybag.itemList.Count ;i++){
+            if(mybag.itemList[i].itemName == keyword)
+                exist = true;
+                Debug.Log("item exist!");
+        }
+    }
+
+}
