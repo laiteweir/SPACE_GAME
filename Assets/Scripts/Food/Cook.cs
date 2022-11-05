aca9324b6c69cb5c3baf8882f4229b969c35aa37
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-05 19:32:17 +0800

    cook system

--- Assets/Scripts/Food/Cook.cs
+++ Assets/Scripts/Food/Cook.cs
@@ -2,27 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckItem : Keyitem
+public class Cook : Keyitem
 {
     // Start is called before the first frame update
     public string keyword;
     public bool exist = false;
     [SerializeField] Inventory mybag;
     public override void KeyitemEvent(){
-        Debug.Log("start check item");
         if(!exist)
             Check();
     }
-    public override void EndKeyitemEvent()
-    {
-        Debug.Log("end check item!");
-    }
-    public void Check(){
+    void Check(){
         for(int i=0; i<mybag.itemList.Count ;i++){
-            if(mybag.itemList[i].itemName == keyword)
+            if(mybag.itemList[i].itemName == keyword){
                 exist = true;
-                Debug.Log("item exist!");
+                Debug.Log("start to cook!");
+                mybag.itemList[i].itemName = keyword + "cooked";
+            }
         }
+        if(!exist)
+            Debug.Log("there is nothing to cook");
     }
-
 }
