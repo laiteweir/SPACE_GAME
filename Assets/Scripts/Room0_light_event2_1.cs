7377883290ed116b976e6dc41f9a46df9f889bf3
g23988 <g23988@gmail.com>
2022-11-07 02:01:51 +0800

    add class room0

--- Assets/Scripts/Room0_light_event2_1.cs
+++ Assets/Scripts/Room0_light_event2_1.cs
@@ -9,9 +9,9 @@ public class Room0_light_event2_1 : Keyitem
     public override void KeyitemEvent()
     {
         Debug.Log("Light1_touch");
-        Manager.Instance.Room0_lights[1-1] = true;
-        Manager.Instance.room0_event2_verify_light_sort(1);
-        Manager.Instance.Room0_event2_light1.GetComponent<Light2D>().color = Color.green;
+        Manager.Instance.room0.Room0_lights[1-1] = true;
+        Manager.Instance.room0.room0_event2_verify_light_sort(1);
+        Manager.Instance.room0.Room0_event2_light1.GetComponent<Light2D>().color = Color.green;
     }
     public override void EndKeyitemEvent()
     {
--- Assets/Scripts/Room0_light_event2_4.cs
+++ Assets/Scripts/Room0_light_event2_4.cs
@@ -10,9 +10,9 @@ public class Room0_light_event2_4 : Keyitem
     public override void KeyitemEvent()
     {
         Debug.Log("Light4_touch");
-        Manager.Instance.Room0_lights[4-1] = true;
-        Manager.Instance.room0_event2_verify_light_sort(4);
-        Manager.Instance.Room0_event2_light4.GetComponent<Light2D>().color = Color.green;
+        Manager.Instance.room0.Room0_lights[4-1] = true;
+        Manager.Instance.room0.room0_event2_verify_light_sort(4);
+        Manager.Instance.room0.Room0_event2_light4.GetComponent<Light2D>().color = Color.green;
     }
     public override void EndKeyitemEvent()
     {
