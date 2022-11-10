5046770e86a448e5c5db716427a8c1316484d5b0
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-11 01:50:49 +0800

    see item

---
+++ Assets/Scripts/Keyitems/Seeitem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeitem : Keyitem
+{
+    
+    public Item Object;
+    public GameObject UI;
+    public GameObject slotgrid;
+    public Slot slotprefab;
+    public static Slot See;
+    bool is_watching = false;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.X)&&is_watching){
+            is_watching = false;
+            EndKeyitemEvent();
+        }
+    }
+
+    public override void KeyitemEvent()
+    {
+        See = Instantiate(slotprefab, slotgrid.transform.position, Quaternion.identity);
+        See.gameObject.transform.SetParent(slotgrid.transform); 
+        Manager.Instance.actionMapPlayer.Disable();
+        See.slotItem = Object;
+        See.slotImage.sprite = Object.itemImage;
+        is_watching = true;
+    }
+
+    public override void EndKeyitemEvent()
+    {
+        Manager.Instance.actionMapPlayer.Enable();
+        Destroy(See);
+    }
+}
