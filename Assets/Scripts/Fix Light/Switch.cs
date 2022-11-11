8332094ec437c24b532ba83a598e6b0bb6e8c820
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-11 19:15:49 +0800

    add light art

--- Assets/Scripts/Fix Light/Switch.cs
+++ Assets/Scripts/Fix Light/Switch.cs
@@ -6,15 +6,18 @@ public class Switch : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject up;
+    public GameObject down;
     public bool is_on = false;
     void Start()
     {
-        
+         up.SetActive(!is_on);
+         down.SetActive(is_on);
     }
 
     // Update is called once per frame
     private void OnMouseUp(){
         up.SetActive(is_on);
+        down.SetActive(!is_on);
         is_on = !is_on;
     }
 }
