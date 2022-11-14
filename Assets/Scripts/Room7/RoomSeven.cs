33beca27ee00007175893c729bbc8943ac1af748
g23988 <g23988@gmail.com>
2022-11-14 18:59:50 +0800

    x

--- Assets/Scripts/Room7/RoomSeven.cs
+++ Assets/Scripts/Room7/RoomSeven.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RoomSeven : MonoBehaviour
 {
+    private bool goDebug = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(goDebug){
+            Manager.Instance.SetDebugMode(goDebug,9,-1);        
+        }
     }
 
     // Update is called once per frame
