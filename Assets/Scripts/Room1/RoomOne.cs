2b1084d3d023066dd46f4e29df5006e89eab93b6
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-16 02:00:02 +0800

    add events

--- Assets/Scripts/Room1/RoomOne.cs
+++ Assets/Scripts/Room1/RoomOne.cs
@@ -7,6 +7,10 @@ public class RoomOne : MonoBehaviour
     // Start is called before the first frame update
     bool isopen = true;
     [SerializeField] GameObject room_1_light;
+    public GameObject room1_event1;
+    public GameObject room1_event2;
+    public GameObject room1_event3;
+    public GameObject room1_event4;
     void Start(){
         Room_1Data.turn_on_light = false;
     }
