03444212b1c8278bc8cc446dcd733cc53ee44687
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-18 22:28:51 +0800

    room5

--- Assets/Scripts/Food/Find_food.cs
+++ Assets/Scripts/Food/Find_food.cs
@@ -8,18 +8,26 @@ public class Find_food : Keyitem
     [SerializeField] Inventory mybag;
     public Item food;
     bool is_find = false;
+    [SerializeField] TextAsset textFile;
+    public GameObject other;
+
+    private string[] dialog;
     public override void KeyitemEvent(){
-        if(is_find)
-            return;
+        
         if(!mybag.itemList.Contains(food)){
             food.itemName = "Food";
             mybag.itemList.Add(food);
+        
+    
+            InventoryManager.CreateNewItem(food);
+            Debug.Log("find some food");
+            is_find = true;
+            dialog = textFile.text.Split('\n');
+            Manager.Instance.ui.SetActive(true);
+            Manager.Instance.dialogBox.TextIsOn = true;
+            Manager.Instance.dialogBox.StartTalk(dialog);
+            gameObject.SetActive(false);
+            other.SetActive(true);
         }
-        else{
-            food.itemHeld +=1 ;
-        }
-        InventoryManager.CreateNewItem(food);
-        Debug.Log("find some food");
-        is_find = true;
     }
 }
