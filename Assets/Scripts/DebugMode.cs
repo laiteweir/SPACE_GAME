582a9d5c62a698619206551c6d4cc408c3428ee9
Wei Liu (RD-TW) <wei_liu@trendmicro.com>
2022-11-19 22:19:59 +0800

    x

--- Assets/Scripts/DebugMode.cs
+++ Assets/Scripts/DebugMode.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DebugMode : MonoBehaviour
 {
-    // Start is called before the first frame updateaaaaaa
-    private bool goDebug = true;
+    // Start is called before the first frame updateaaa
+    private bool goDebug = false;
     private bool global_light_on = false;
     private float x = 16.56f;
     private float y = 13.56f;
