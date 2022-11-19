cfe9d788d5dd9b7697805051dd6b810ceccbc72f
Hartmann <jeffreylin8902@gmail.com>
2022-11-19 21:23:09 +0800

    a

--- Assets/Scripts/Room3/RoomThree.cs
+++ Assets/Scripts/Room3/RoomThree.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class RoomThree : MonoBehaviour
 {
+    public GameObject room3_event1;
+    public GameObject room0_event2_1;
+    public GameObject room0_event2_2;
+    public GameObject room0_event2_3;
+    public GameObject room0_event2_4;
+    public GameObject room0_event2_5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,9 @@ public class RoomThree : MonoBehaviour
     void Update()
     {
         
+    }
+    public bool Trun_On_Light()
+    {
+        return true;
     }
 }
