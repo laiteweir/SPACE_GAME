d62529d29e720212394ddadbd6bbda9601a05d75
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-23 17:43:02 +0800

    add boss event

---
+++ Assets/Scripts/BossFight/Slime2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime2 : MonoBehaviour
+{
+    private Rigidbody2D _myRD {get;set;}
+    public float speed = 300f;
+    public bool is_attack = false;
+    
+
+    private void Awake(){
+        _myRD = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        Invoke(nameof(SetRandomTrajectory),0.5f);
+    }
+    
+    
+
+    // Update is called once per frame
+    void SetRandomTrajectory(){
+        Vector2 force = Vector2.zero;
+        force.x = Random.Range(-0.5f,0.5f);
+        force.y = -1;
+        _myRD.AddForce(force.normalized*speed);
+    }
+}
