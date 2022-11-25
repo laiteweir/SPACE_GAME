7f42f7cb0ab36cdf13aed3d8c0232b86a93815c6
g23988 <g23988@gmail.com>
2022-11-26 02:18:51 +0800

    add setItem method

--- Assets/Scripts/Room9/RoomNine.cs
+++ Assets/Scripts/Room9/RoomNine.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class RoomNine : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Dictionary<string, bool> items = new Dictionary<string, bool>();
     void Start()
     {
-        
+        items.Add("RedFlower",false);
+        items.Add("YellowLeaf",false);
+        items.Add("Hair",false);
+        items.Add("WhiteJar",false);
+        items.Add("Folder",false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public bool setItem(string itemName){
+        bool result = false;
+        int dirCount = items.Count;
+        int trueCount = 0;
+        if(items.ContainsKey(itemName)){
+            items[itemName] = true;
+        }
+        foreach(KeyValuePair<string, bool> kvp in items ){
+            if (kvp.Value == true){
+                trueCount = trueCount +1;
+            }
+        }
+        if(trueCount == dirCount){
+            result = true;
+        }
+        
+        return result;
     }
 }
