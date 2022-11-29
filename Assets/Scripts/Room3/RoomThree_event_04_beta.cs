fc50e320e16b5941af214aa0778aef6945e6c8b9
Hartmann <jeffreylin8902@gmail.com>
2022-11-29 11:26:02 +0800

    a

---
+++ Assets/Scripts/Room3/RoomThree_event_04_beta.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomThree_event_04_beta : Keyitem
+{
+    [SerializeField] TextAsset file;
+    private string[] dialog;
+    private bool is_trigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialog = file.text.Split("\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Manager.Instance.dialogBox.TextIsOn == false && is_trigger == true)
+        {
+            
+            Manager.Instance.room3.room3_event2_1.SetActive(true);
+            Manager.Instance.room3.room3_event2_2.SetActive(true);
+            Manager.Instance.room3.room3_event2_3.SetActive(true);
+            Manager.Instance.room3.room3_event2_4.SetActive(true);
+            Manager.Instance.room3.room3_event2_5.SetActive(true);
+            Manager.Instance.room3.room3_event2_5.SetActive(true);
+            Manager.Instance.room3.room3_event4.SetActive(false);
+        }
+    }
+
+    public override void KeyitemEvent()
+    {
+        Manager.Instance.ui.SetActive(true);
+        Manager.Instance.dialogBox.TextIsOn = true;
+        Manager.Instance.dialogBox.StartTalk(dialog);
+        is_trigger = true;
+
+
+
+    }
+    public override void EndKeyitemEvent()
+    {
+        // Debug.Log("test fire");
+    }
+}
