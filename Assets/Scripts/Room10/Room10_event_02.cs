1e483b351f519b2a4dc856f4b087a7f7f5bd2d4d
laiteweir <85702611+laiteweir@users.noreply.github.com>
2022-12-02 07:07:59 +0800

    12/2  Adjust room2 & room10

--- Assets/Scripts/Room10/Room10_event_02.cs
+++ Assets/Scripts/Room10/Room10_event_02.cs
@@ -20,6 +20,7 @@ public class Room10_event_02 : MonoBehaviour
         {
             // Debug.Log("You have fixed both engines!");
             Manager.Instance.room10.room10_bigLight.enabled = true;
+            Manager.Instance.room2.room2_bigLight.enabled = true;
             Manager.Instance.ui.SetActive(true);
             Manager.Instance.dialogBox.TextIsOn = true;
             Manager.Instance.dialogBox.StartTalk(dialog);
