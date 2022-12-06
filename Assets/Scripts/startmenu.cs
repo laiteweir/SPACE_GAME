226c2e9983c7962e270547b600dbc883b59f9a33
Hartmann <jeffreylin8902@gmail.com>
2022-12-06 20:39:16 +0800

    a

---
+++ Assets/Scripts/startmenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject image;
+    public void Start()
+    {
+        image.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Start() 
+    {
+        
+    }
+    public void Tutorial()
+    {
+
+    }
+    public void Setting()
+    {
+
+    }
+}
